bbed66ce33d9437bdcbfe78afddc14a2a290d178
LaliteshMuddapati <lallivedu@gmail.com>
2025-03-26 09:17:02 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+n1=int(input("Enter the first number: "))
+n2=int(input("Enter the second number: "))
+n3=int(input("Enter the third number"))
+
+print("Before swapping: ", n1,n2,n3)
+
+print("After swapping: ", n2,n3,n1)
